258f2c3a611ee3c7fed2799363d6da0c1e10e752
Samuel Tijani <omalitijani@gmail.com>
2022-01-11 13:58:34 +0100

    Create task lists

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_app/models/task.dart';
 import 'package:todoey_app/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
-    );
+    return ListView
   }
 }
