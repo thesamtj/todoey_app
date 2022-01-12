22582a20afded76e6e9c0e206082d30b30ac8461
Samuel Tijani <omalitijani@gmail.com>
2022-01-12 23:36:22 +0100

    Add callback

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -4,15 +4,18 @@ class TaskTile extends StatelessWidget {
   final bool isChecked;
   final String taskTitle;
   final void Function(bool?) checkboxCallback;
+  final void Function() longPressCallback;
 
   TaskTile(
       {required this.isChecked,
       required this.taskTitle,
-      required this.checkboxCallback});
+      required this.checkboxCallback,
+      required this.longPressCallback});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onLongPress: longPressCallback,
       title: Text(
         taskTitle,
         style: TextStyle(
