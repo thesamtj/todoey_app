516d75b5517772da795bba54565cfe52e9e46d20
Samuel Tijani <omalitijani@gmail.com>
2022-01-12 22:59:05 +0100

    Fix bug

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -21,12 +21,7 @@ class TasksScreen extends StatelessWidget {
               child: Container(
                 padding: EdgeInsets.only(
                     bottom: MediaQuery.of(context).viewInsets.bottom),
-                child: AddTaskScreen((newTaskTitle) {
-                  // setState(() {
-                  //   tasks.add(Task(name: newTaskTitle));
-                  // });
-                  Navigator.pop(context);
-                }),
+                child: AddTaskScreen(),
               ),
             ),
           );
